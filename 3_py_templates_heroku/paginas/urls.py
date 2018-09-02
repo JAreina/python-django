339bb55b257b8d2c35277097bc877782ea8b8375
JAreina <jareinafdez@gmail.com>
2018-09-02 20:19:10 +0200

    borrado heroku

--- 3_py_templates_heroku/paginas/urls.py
+++
@@ -1,7 +0,0 @@
-# pages/urls.py
-from django.urls import path
-from . import views
-urlpatterns = [
- path('', views.Inicio.as_view(), name='inicio'),
- path('sobremi/',views.SobreMi.as_view(), name='SOBRE MI')
-]
