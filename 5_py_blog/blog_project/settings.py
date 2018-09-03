2da9c97d064916e4e0061200864b5fdaad3ca5ed
JAreina <jareinafdez@gmail.com>
2018-09-03 19:45:34 +0200

    urls, views y plantillas html blog

--- 5_py_blog/blog_project/settings.py
+++ 5_py_blog/blog_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
