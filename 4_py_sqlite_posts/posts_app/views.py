1e4bb82c05f6b284155fb3336ac0344eb656b79a
JAreina <jareinafdez@gmail.com>
2018-09-03 09:02:10 +0200

    template, views, urls

--- 4_py_sqlite_posts/posts_app/views.py
+++ 4_py_sqlite_posts/posts_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Post # modelo que vamos a usar 
+
+class Inicio(ListView):
+    model = Post
+    template_name = "home.html"
--- 4_py_sqlite_posts/posts_proyecto/settings.py
+++ 4_py_sqlite_posts/posts_proyecto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'posts_proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
