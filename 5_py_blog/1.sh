c350d9a64bb383c957d783ff807352060ffb1c25
JAreina <jareinafdez@gmail.com>
2018-09-03 19:17:37 +0200

    modelos post

--- 5_py_blog/1.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-pipenv install django==2.1.1 &&
-pipenv shell
-
--- crear_un_proyecto_diango_con_bash/2.sh
+++ crear_un_proyecto_diango_con_bash/2.sh
@@ -1,19 +1,19 @@
 #!bin/bash
 nombre_proyecto=$1
-
 nombre_app=$2
 
+if [ -z ${nombre_proyecto} ] || [ -z ${nombre_app} ]; then
 
-echo ${nombre_proyecto}
+   echo "*******************************************"
+   echo "USO : bash 2.sh nombre_proyecto nombre_app "
+   echo "*******************************************"
+   echo "*******************************************"
 
-if [ -n ${nombre_proyecto} ]; then
+else
  django-admin startproject "${nombre_proyecto}_project" . 
-fi
-if [ -n ${nombre_app} ]; then
  python manage.py startapp "${nombre_app}_app" 
  python manage.py migrate
+ python manage.py runserver
+
 fi
 
-if [ -n ${nombre_proyecto} ] &&  [ -n ${nombre_app} ] ;then
- python manage.py runserver
-fi
