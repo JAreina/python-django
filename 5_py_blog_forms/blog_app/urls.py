d6cf691ac3f497d17cf08966b914241d3197a00b
JAreina <jareinafdez@gmail.com>
2018-09-05 17:51:53 +0200

    delete post

--- 5_py_blog_forms/blog_app/urls.py
+++ 5_py_blog_forms/blog_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 path('', views.ListBlogVista.as_view(), name='home'),
 path('post/<int:pk>/', views.ListBlogDetalle.as_view(), name='postdetalle'),
-path('post/nuevo/', views.CrearNuevo.as_view(),name='nuevo'),
+path('post/nuevo/', views.CrearNuevo.as_view(),name='postnuevo'),
 path('post/<int:pk>/edit/',views.ActualizarPost.as_view(), name='postedit'),
+path('post/<int:pk>/delete/',views.BorrarPost.as_view(), name='postdelete'),
 ]
--- 5_py_blog_forms/blog_app/views.py
+++ 5_py_blog_forms/blog_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 
 from . models import Post
-
+from django.urls import reverse_lazy
 
 class ListBlogVista(ListView):
     model = Post
@@ -17,10 +17,15 @@ class ListBlogDetalle(DetailView):
     
 class CrearNuevo(CreateView):
     model = Post
-    template_name = "nuevo.html"
+    template_name = "post_nuevo.html"
     fields = '__all__'
 
 class ActualizarPost(UpdateView):
     model = Post
     fields = ['titulo', 'texto']
     template_name = 'post_edit.html'
+
+class BorrarPost(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
